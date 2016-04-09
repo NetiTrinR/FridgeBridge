25524a60dd5cb85ca2405329d6530ec24fd97749
Michael Manning <michaelmanning@gmail.com>
2016-04-08 21:00:28 -0600

    Initial database designs

---
+++ app/Inven.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Fridge;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Inven extends Model
+{
+    //
+}
---
+++ database/migrations/2016_04_09_020533_create_invens_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invens', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('item_id')->unsigned();
+            $table->integer('quantity')->unsigned()->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('invens');
+    }
+}
