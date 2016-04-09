cc7e38cc6b0b15ec7ccb9a82c67e9bb82974bba7
Michael Manning <michaelmanning@gmail.com>
2016-04-09 14:35:37 -0600

    Updated views and fixed a bug

--- resources/views/partials/recipe.blade.php
+++ resources/views/partials/recipe.blade.php
@@ -1,9 +1,20 @@
-<div class="row well">
-    <div class="col-xs-3">
-        <img src="{{ $recipe['image'] }}" alt="{{ $recipe['label'] }} Recipe">
+<!-- <div class="row ">
+    <div class="col-xs-12">
+
     </div>
-    <div class="col-xs-9">
-        <h3><a href="{{ $recipe['url'] }}">{{ $recipe['label'] }}</a></h3>
-        {{ count(array_match($inFridge, $foods)) }} / {{ count($foods) }}
+</div> -->
+
+<div class="panel panel-default">
+    <div class="panel-body">
+        <div class="row">
+            <div class="col-xs-3">
+                <a href="{{ $recipe['url'] }}" target="_blank"><img src="{{ $recipe['image'] }}" alt="{{ $recipe['label'] }} Recipe" class="img-responsive img-thumbnail"></a>
+            </div>
+            <div class="col-xs-9">
+                <h3><a href="{{ $recipe['url'] }}" target="_blank">{{ $recipe['label'] }}</a></h3>
+                {{ count(array_match($inFridge, $foods)) }} / {{ count($foods) }}
+            </div>
+
+        </div>
     </div>
 </div>
