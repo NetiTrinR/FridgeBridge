be178f684a113a755d9537ec00613b1800113983
Michael Manning <michaelmanning@gmail.com>
2016-04-09 00:13:40 -0600

    Added item routes

---
+++ resources/views/item/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-md-6">Search</div>
+            <div class="col-xs-12 col-md-6">Sort</div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <ul class="list-group">
+                    <li class="list-group-item">List Groups Items</li>
+                    <li class="list-group-item">List Groups Items</li>
+                    <li class="list-group-item">List Groups Items</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+@endsection
