e92cd8d309ae94f58039c9198a1a5f080e7dcda6
Michael Manning <michaelmanning@gmail.com>
2016-04-08 23:19:07 -0600

    Added testing logins

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,5 +14,22 @@ class UsersTableSeeder extends Seeder
     {
         DB::table('users')->truncate();
         factory(User::class, 10)->create();
+        DB::table('users')->insert([
+            [
+                'name' => 'Michael Manning',
+                'email' => 'admin1@example.com',
+                'password' => bcrypt('123456'),
+            ],
+            [
+                'name' => 'Sean White',
+                'email' => 'admin2@example.com',
+                'password' => bcrypt('123456'),
+            ],
+            [
+                'name' => 'Elias Athey',
+                'email' => 'admin3@example.com',
+                'password' => bcrypt('123456'),
+            ],
+        ]);
     }
 }
